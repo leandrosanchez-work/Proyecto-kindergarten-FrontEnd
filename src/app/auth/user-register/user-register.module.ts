d8153798bf9ae185f30ded9fdb43a3388ec052bb
Leandro Sanchez <leandro.sanchez.747@gmail.com>
2024-10-07 12:41:17 -0300

    router configuration with the route 'register'

---
+++ src/app/auth/user-register/user-register.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserRegisterRoutingModule } from './user-register-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserRegisterRoutingModule
+  ]
+})
+export class UserRegisterModule { }
